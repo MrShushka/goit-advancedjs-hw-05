32f35314860f8f36fc323e856ac57faa640d53cd
MrShushka <51093788+MrShushka@users.noreply.github.com>
2025-03-29 14:15:54 +0200

    initial commit

---
+++ ts-courses/dist/concatenation.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.concatenation = concatenation;
+function concatenation(firstWord, secondWord) {
+    console.log(`${firstWord} ${secondWord}`);
+}
+//# sourceMappingURL=concatenation.js.map
